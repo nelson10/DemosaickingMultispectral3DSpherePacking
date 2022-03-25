258e62892eff4126e8b4f26fe75cba85059e2958
nelson10 <38512291+nelson10@users.noreply.github.com>
2022-03-24 19:52:22 -0500

    Add files via upload

--- Fig/figure5.m
+++ Fig/figure5.m
@@ -2,45 +2,15 @@
 % Post-doctorado Pontícia Universidad Católica de Valparaíso (PUCV)
 % Date 2 February 2022
 
-function [NF]=figure5(N,NF,mask,dist)
+function [NF]=figure5(N,NF)
 %% Comparison 3D random and optimal spheres
 %NF = 9;
 %N = 256;
 set(groot, 'defaultFigureUnits', 'centimeters', 'defaultFigurePosition', [0 0 30.86 30.8]);
-
-if(NF ==5)
-    A = [1 5 2 5;4 3 4 3;2 5 1 5;4 3 4 3]; % BTES5
-elseif(NF ==6)
-    A = [1 5 2 5;6 3 6 4;2 5 1 5;6 4 6 3]; % BTES6
-elseif(NF ==7)
-    A = [1 7 2 7;5 3 6 4;2 7 1 7; 6 4 5 3]; % BTES7
-elseif(NF ==8)
-    A = [1 7 2 8;5 3 6 4;2 8 1 7;6 4 5 3]; % BTES8
-elseif(NF ==9)
-    A = [1 8 3 9;6 4 7 5;3 9 2 8; 7 5 6 4]; % BTES9
-elseif(NF ==10)
-    A = [1 9 4 10;7 5 8 6;3 10 2 9;8 6 7 5]; % BTES10
-elseif(NF ==11)
-    A = [1 10 4 11;8 5 9 7;3 11 2 10;9 7 8 6]; % BTES11
-elseif(NF ==12)
-    A = [1 11 4 12;9 5 10 7;3 12 2 11;10 8 9 6]; % BTES12
-elseif(NF ==13)
-    A = [1 12 4 13;9 5 11 7;3 13 2 12;11 8 10 6]; % BTES13
-elseif(NF ==14)
-    A = [1 13 4 14;9 5 11 7;3 14 2 13;12 8 10 6]; % BTES14
-elseif(NF ==15)
-    A = [1 13 4 15;9 5 11 7;3 15 2 14;12 8 10 6]; % BTES15
-elseif(NF == 16)
-    A = [1,13,4,15;9,5,11,7;3,16,2,14;12,8,10,6]; % BTES16
-end
-
-if(NF <= 16)
-    B = ones(N/4,N/4);
-    G = kron(B,A);
-end
+code = 1;
+[G] = codedPatterns(N,NF,code);
 [T] = distance(G);
 G = G(1:NF,1:NF);
-m = max(T(:));
 minimum = min(T(:));
 
 fontsize = 18;
@@ -50,9 +20,11 @@ fig=figure(1);
 subplot(1,2,1)
 showSphere(G,radius,fontsize); title("BTES"+num2str(NF)+" distance="+num2str(minimum), 'FontSize', fontsize)
 set(gca,'FontSize',fontsize)
-
-G = mask(1:NF,1:NF);
-minimum = dist;
+code = 6;
+[G] = codedPatterns(N,NF,code);
+G = G(1:NF,1:NF);
+[T] = distance(G);
+minimum = min(T(:));
 
 radius = minimum/2;
 subplot(1,2,2)
