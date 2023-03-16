fc3a241ccb1034790d08dc7e250d7a7d7f8154bf
nelson10 <38512291+nelson10@users.noreply.github.com>
2023-03-16 19:04:59 -0300

    Add files via upload

---
+++ MainFigures.m
@@ -0,0 +1,18 @@
+%% This main reproduce all the figures in the following paper: Multispectral Filter Array Design by Optimal Sphere Packing
+% summited for publication in Transaction on Image Processing 
+% Authors: Nelson Diaz, Alejandro Alvarado, Pablo Meza, Felipe Guzman, Esteban Vera
+
+clear all;
+clc;
+close all;
+%% Add path to the dataset
+addpath(genpath('./Dataset'));
+addpath(genpath('./figures'));
+addpath(genpath('./src'));
+
+figure2(); % Fig 2: sphere packing upper bound 
+figure3(); % Fig 3: MSFA patterns
+figure4(); % Fig 4: sphere packing 16 filters
+figure5(); % Fig 5: sphere packing 25 filters
+figure6(); % Fig 6: spatial accuracy
+figure7(); % Fig 7: spectral accuracy
