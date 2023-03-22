e99d413f89620a6d69e2de20b0663073f909524f
nelson10 <38512291+nelson10@users.noreply.github.com>
2023-03-22 13:45:47 -0300

    Add files via upload

--- figures/figure44.m
+++ figures/figure44.m
@@ -2,34 +2,36 @@
 % Post-doctorado Pontícia Universidad Católica de Valparaíso (PUCV)
 % Date 17 May 2022
 
-function [NF]=figure25(N,NF)
+function [NF]=figure55(N,NF)
 %% Comparison 3D random and optimal spheres
 %NF = 9;
 %N = 256;
+
+
+%fig = figure('Renderer', 'painters', 'Position', [10 10 1500 1200]);
 set(groot, 'defaultFigureUnits', 'centimeters', 'defaultFigurePosition', [0 0 100.86 30.8]);
 fontsize = 18;
-fig=figure(5);
-cd=[5,7];
-t = tiledlayout(1,2,'TileSpacing','Compact','Padding','Compact');
-x = ["                                        (a)","                                        (b)"];
-for i=1:2
-    code = cd(i); % 2 Brauers, 4 Uniform
+fig=figure(4);
+x = ["(a)                        ","(b)                        ","(c)                        ","(d)                        ","(e)                        ","(f)                        ","(g)                        ","(h)                        "];
+t = tiledlayout(2,4,'TileSpacing','Compact','Padding','Compact');
+for i=1:8
+    code = i-1; % 2 Brauers, 4 Uniform
     [G] = codedPatterns(N,NF,code);
-    [textcode] =checkCode2(code);
+    %[textcode] =checkCode2(code);
     G = G(1:NF,1:NF);
     [T] = distance(G);
     minimum = min(T(:));
     radius = minimum/2;
     [density,diameter]= ComputeDensity(minimum,NF,G);
     density = round(density,2);
-    minimum = round(minimum,2);
-    %    annotation('textbox',[.9 .5 .1 .2], ...
-    %'String','Text outside the axes','EdgeColor','none')
+    minimum = diameter;%round(minimum,2);
+    
+    %subplot(2,4,i)
+    %imagesc(G)
     nexttile
     showSphereGeneral(G,radius,fontsize); 
-    %title("d_{"+textcode+"}="+num2str(minimum)+" \rho_{"+textcode+"}="+num2str(density), 'FontSize', fontsize,'Fontname','Times');
     title("d="+num2str(minimum)+" \rho="+num2str(density), 'FontSize', fontsize,'Fontname','Times');
-    xlabel(['X',x(i)], 'FontSize', fontsize,'Fontname','Times');
+    xlabel([x(i)], 'FontSize', fontsize,'Fontname','Times');
     ylabel('Y', 'FontSize', fontsize,'Fontname','Times');
     zlabel('\lambda', 'FontSize', fontsize,'Fontname','Times');
     axis equal;
@@ -37,11 +39,10 @@ for i=1:2
     hold on
     %view(40,35)
     grid on;
-    %set(get(gca,'title'),'Position',[25.5 -8 1.00011])
     set(gca,'FontSize',fontsize,'Fontname','Times')
 end
 orient(fig,'landscape')
-%print(fig,'figure25.png','-dpng','-r200')
-print(fig,'figure5.eps','-depsc','-r400')
-%print(fig,'figure25.pdf','-dpdf','-r400')
+
+%print(fig,'figure1.png','-dpng','-r400') 
+print(fig,'figure4.eps','-depsc','-r400')
 end
