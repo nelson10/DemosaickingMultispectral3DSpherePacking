bfd1f43cc3173302654cfc543b6c6243d4858caa
nelson10 <38512291+nelson10@users.noreply.github.com>
2023-03-22 15:54:46 -0300

    Add files via upload

--- src/ComputeDensity.m
+++ src/ComputeDensity.m
@@ -1,24 +1,10 @@
 function [density,diameter]= ComputeDensity(dist,NF,G)
-[r,c,z1] = find(G);
-X = [r c z1]'; % generator matrix
-D = pdist(X');
-D1 = squareform(D);
-R = zeros(numel(G),1);
-for i=1:numel(G)
-     temp = D1(i,:);
-     [x2,y2,z2]=find(temp);
-     R(i)= min(z2)/2;
-end
-
 cube = (NF+1).^3;
-%dist = dist./2;
-sphere = 0;
-for i =1:numel(G)
-    sphere = sphere + (4/3).*pi.*(R(i).^3);
-end
+dist = dist./2;
+sphere = (NF.^2).*(4/3).*pi.*(dist.^3);
 density = (sphere./cube);
-diameter = mean(R(:))*2;
-disp("Sphere Packing Density "+ density + " diameter "+ diameter);
+disp("Sphere Packing Density "+ density)
+diameter = dist *2;
 
 % nb =NF.^2;
 % dis = 2*(((NF+1).^3)./(4*nb*sqrt(2))).^(1/3);
---
+++ src/ComputeDensityGeneral.m
@@ -0,0 +1,29 @@
+function [density,diameter]= ComputeDensityGeneral(dist,NF,G)
+[r,c,z1] = find(G);
+X = [r c z1]'; % generator matrix
+D = pdist(X');
+D1 = squareform(D);
+R = zeros(numel(G),1);
+for i=1:numel(G)
+     temp = D1(i,:);
+     [x2,y2,z2]=find(temp);
+     R(i)= min(z2)/2;
+end
+
+cube = (NF+1).^3;
+%dist = dist./2;
+sphere = 0;
+for i =1:numel(G)
+    sphere = sphere + (4/3).*pi.*(R(i).^3);
+end
+density = (sphere./cube);
+diameter = mean(R(:))*2;
+disp("Sphere Packing Density "+ density + " diameter "+ diameter);
+
+% nb =NF.^2;
+% dis = 2*(((NF+1).^3)./(4*nb*sqrt(2))).^(1/3);
+% dis = dis./2;
+% sphere = (NF.^2).*(4/3).*pi.*(dis.^3);
+% density1 = (sphere./cube);
+% disp("Optimal Sphere Packing Density "+ density1)
+end
