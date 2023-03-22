40827828730a8aecf66f528e02217138ce711b68
nelson10 <38512291+nelson10@users.noreply.github.com>
2023-03-22 16:23:47 -0300

    Add files via upload

--- src/checkCode2.m
+++ src/checkCode2.m
@@ -1,15 +1,15 @@
 function [textcode] =checkCode2(code)
 if(code==0)
-    textcode ="Random";
+    textcode ="RND";
 elseif(code ==1)
     textcode ="BN";
 elseif(code ==2)
-    textcode ="Brauers"; % (Brauers and Aach, 2006)
+    textcode ="BRA"; % (Brauers and Aach, 2006)
     %Brauers, Johannes, and Til Aach. "A color filter array based multispectral camera." 12. Workshop Farbbildverarbeitung. Ilmenau, 2006.
 elseif(code ==3)
-    textcode ="Seq";
+    textcode ="SEQ";
 elseif(code ==4)
-    textcode ="Uniform";
+    textcode ="UNIF";
 elseif(code ==5)
     textcode ="IMEC";
 elseif(code == 6)
