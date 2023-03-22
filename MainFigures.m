97cd598d96e67b6dfaf72b3ef33bdc0247b503b3
nelson10 <38512291+nelson10@users.noreply.github.com>
2023-03-22 15:54:06 -0300

    Add files via upload

--- MainFigures.m
+++ MainFigures.m
@@ -12,9 +12,13 @@ addpath(genpath('./src'));
 
 figure2(); % Fig 2: sphere packing upper bound 
 figure3(); % Fig 3: MSFA patterns
-figureS2();% Fig S2: Coded aperture 1st band, Supplementary material
 figure4(); % Fig 4: sphere packing 16 filters
 figure5(); % Fig 5: sphere packing 25 filters
+
 figure6(); % Fig 6: spatial accuracy
 figure7(); % Fig 7: spectral accuracy
+%% Suplementary material
+figureS2();% Fig S2: Coded aperture 1st band, Supplementary material
+figureS3(); % Fig S3: irregular sphere packing 16 filters
+figureS4(); % Fig S4: irregular sphere packing 25 filters
 %figure3fourier(); % % Fig 3: MSFA patterns in Fourier
