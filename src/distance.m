e8be3ceba58c92b3ffb56bb41cb4dd8e28302960
nelson10 <38512291+nelson10@users.noreply.github.com>
2023-04-05 15:15:04 -0400

    Add files via upload

--- src/distance.m
+++ src/distance.m
@@ -6,17 +6,10 @@
 % Solution using the Discrete Sphere Packing based on 3D N^2 Queens
 % Approach
 
-function [T] = distance(G)
+function [NAlpha] = distance(G)
 [~,N]= size(G);
-L = round(sqrt(N));
-G = G(1:L,1:L);
 [r,c,z] = find(G);
 B = [r c z];
-T = zeros(L,L);
 D = pdist(B);
-dist = squareform(D);
-for i=1:L^2
-    [~,~,values]=find(dist(i,:));
-    T(r(i),c(i)) = min(values);
-end
+NAlpha = min(D(:)); % minimal value between <v,v> for all v \in r
 end
